71c5d573bd66b853d90023f5b7b9485c37930bbe
Anton Baronin <webantonio@yandex.ru>
2025-04-01 23:58:11 +0300

    PR5 check-l

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,31 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+const newCards = document.querySelector("#card-template").content;
+const placesList = document.querySelector(".places__list");
+
+function addCard(item, deleteCard) {
+  const newPlacesItem = newCards.querySelector(".places__item").cloneNode(true);
+  newPlacesItem.querySelector(".card__image").src = item.link;
+  newPlacesItem.querySelector(".card__image").alt = item.name;
+  newPlacesItem.querySelector(".card__title").textContent = item.name;
+
+  newPlacesItem
+    .querySelector(".card__delete-button")
+    .addEventListener("click", function (evt) {
+      document.querySelector(".card__delete-button").closest(".places__item");
+      deleteCard(newPlacesItem);
+    });
+
+  return newPlacesItem;
+}
+
+function deleteCard(card) {
+  card.remove();
+}
+
+initialCards.forEach((item) => {
+  const cardElement = addCard(item, deleteCard);
+  placesList.append(cardElement);
+});
